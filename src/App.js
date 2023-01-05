24cc8364096c45ca9446821c5149b01220f128ca
KarlKo1 <kongopoiss@gmail.com>
2023-01-05 12:54:44 +0200

    Added uuid, states and songs data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./styles/app.scss";
 import Player from "./components/Player";
 import Song from "./components/Song";
+import data from "./utils/data";
+import { useState } from "react";
 
 function App() {
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
   return (
     <div className="App">
-      <Song />
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   );
